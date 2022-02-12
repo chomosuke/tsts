3d62901033c1f6b0752de839a02a4f0e8b0a9e10
chomosuke <a13323600@gmail.com>
2022-02-12 20:26:13 +1100

    4 indentation, made vscode work with yarn

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import * as ts from "typescript";
+import ts from 'typescript';
 
 const source = "let x: string  = 'string'";
 
-let result = ts.transpileModule(source, { compilerOptions: { module: ts.ModuleKind.CommonJS }});
+const result = ts.transpileModule(
+    source,
+    { compilerOptions: { module: ts.ModuleKind.CommonJS } },
+);
 
 console.log(JSON.stringify(result));
